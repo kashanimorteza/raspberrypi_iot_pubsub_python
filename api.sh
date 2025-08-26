ff64bbb869d7dc07f9fb338672fc6f27aa505ab6
morteza <kashani.morteza@gmail.com>
2025-08-27 00:39:01 +0330

    some change

--- api.sh
+++ api.sh
@@ -42,12 +42,7 @@ service_create_all()
     #----------Header
     echo -e "${YELLOW}service_create_all\n$LINE3${ENDCOLOR}"
 
-    service_create_webapi
-    service_create_nginx_create_api
-    service_create_nginx_create_gui
-    service_create_interrupt_gpio
-    service_create_interrupt_1w
-    service_cron_min
+    service_create_gpio_subscriber
 }
 
 #-----------------------------service_status
@@ -56,7 +51,7 @@ service_status_all()
     #----------Header
     echo -e "${YELLOW}service_status\n$LINE3${ENDCOLOR}"
     #----------Variable
-    SERVICES=("iptables" "nginx" "${name}_webapi.service" "${name}_interrupt_gpio.service" "${name}_interrupt_1w.service" "${name}_cron_min.timer")
+    SERVICES=("${name}_gpio_subscriber.service")
     #----------Action
     for service in "${SERVICES[@]}"; do
         if systemctl is-active --quiet "$service"; then
@@ -73,7 +68,7 @@ service_remove_all()
     #----------Header
     echo -e "${YELLOW}service_remove_all\n$LINE3${ENDCOLOR}"
     #----------Variable
-    SERVICES=("iptables" "nginx" "${name}_webapi.service" "${name}_interrupt_gpio.service" "${name}_interrupt_1w.service" "${name}_cron_min.timer")
+    SERVICES=("${name}_gpio_subscriber.service")
     #----------Action
     for service in "${SERVICES[@]}"; do
         echo -e "${BLUE}$service${ENDCOLOR}"
@@ -89,7 +84,7 @@ service_stop_all()
     #----------Header
     echo -e "${YELLOW}service_stop_all\n$LINE3${ENDCOLOR}"
     #----------Variable
-    SERVICES=("iptables" "nginx" "${name}_webapi.service" "${name}_interrupt_gpio.service" "${name}_interrupt_1w.service" "${name}_cron_min.timer")
+    SERVICES=("${name}_gpio_subscriber.service")
     #----------Action
     for service in "${SERVICES[@]}"; do
         echo -e "${BLUE}$service${ENDCOLOR}"
@@ -103,7 +98,7 @@ service_restart_all()
     #----------Header
     echo -e "${YELLOW}service_stop_all\n$LINE3${ENDCOLOR}"
     #----------Variable
-    SERVICES=("iptables" "nginx" "${name}_webapi.service" "${name}_interrupt_gpio.service" "${name}_interrupt_1w.service" "${name}_cron_min.timer")
+    SERVICES=("${name}_gpio_subscriber.service")
     #----------Action
     for service in "${SERVICES[@]}"; do
         echo -e "${BLUE}$service${ENDCOLOR}"
@@ -111,256 +106,32 @@ service_restart_all()
     done
 }
 
-#-----------------------------service_create_webapi
-service_create_webapi()
+#-----------------------------service_create_gpio_subscriber
+service_create_gpio_subscriber()
 {
-    echo -e "${YELLOW}service_create_webapi\n$LINE3${ENDCOLOR}"
+    echo -e "${YELLOW}service_create_gpio_subscriber\n$LINE3${ENDCOLOR}"
 
     #----------Variable
-    api_host=$webapi_host
-    api_port=$webapi_port
-    api_workers=$webapi_workers
+    service_name=gpio_subscriber
 
     #----------webapi
-    echo -e "${BLUE}$name"_"webapi${ENDCOLOR}"
+    echo -e "${BLUE}$name"_"${service_name}${ENDCOLOR}"
     echo """[Unit]
-    Description=$name"_"webapi
+    Description=$name"_"${service_name}
 
     [Service]
     User=root
     WorkingDirectory=$path/
-    ExecStart=$path/.myenv3/bin/uvicorn api:app --host $api_host --port $api_port --workers $api_workers
+    ExecStart=python3 $path/subscriber/gpio.py
     SuccessExitStatus=143
     TimeoutStopSec=10
     Restart=on-failure
     RestartSec=65
 
     [Install]
-    WantedBy=multi-user.target""" > /etc/systemd/system/$name"_"webapi.service
+    WantedBy=multi-user.target""" > /etc/systemd/system/${name}"_"${service_name}.service
 
     systemctl daemon-reload
-    systemctl enable $name"_"webapi
-    systemctl restart $name"_"webapi
-}
-
-#-----------------------------service_create_nginx_create_api
-service_create_nginx_create_api()
-{
-    #----------Header
-    echo -e "${YELLOW}nginx_create_api\n$LINE3${ENDCOLOR}"
-    #----------Variable
-    host=$nginx_api_host
-    port=$nginx_api_port
-    key=$nginx_api_key
-    api_host=$webapi_host
-    api_port=$webapi_port
-    #----------Action
-    echo "server {
-    listen $port;
-    server_name _;
-
-        location /$key {
-            proxy_pass http://$api_host:$api_port;
-            proxy_set_header Host \$host;
-            proxy_set_header X-Real-IP \$remote_addr;
-            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto \$scheme;
-
-            proxy_ssl_verify off;
-
-            proxy_connect_timeout 60s;
-            proxy_send_timeout 60s;
-            proxy_read_timeout 60s;
-            send_timeout 60s;
-        }
-    }" > /etc/nginx/sites-available/$name"_"api.conf
-    ln -s /etc/nginx/sites-available/$name"_"api.conf /etc/nginx/sites-enabled/
-    nginx -t
-    systemctl reload nginx
-    systemctl restart nginx
-}
-
-#-----------------------------service_create_nginx_create_gui
-service_create_nginx_create_gui()
-{
-    #----------Header
-    echo -e "${YELLOW}nginx_create_gui\n$LINE3${ENDCOLOR}"
-    #----------Variable
-    host=$nginx_gui_host
-    port=$nginx_gui_port
-    key=$nginx_gui_key
-    #----------Files
-    echo -e "rm -fr /var/www/$name"_"gui"
-    rm -fr /var/www/$name"_"gui
-    echo -e "mkdir -p /var/www/$name"_"gui/"
-    mkdir -p /var/www/$name"_"gui/
-    echo -e "chown -R $USER:$USER /var/www/$name"_"gui/"
-    chown -R $USER:$USER /var/www/$name"_"gui/
-    echo -e "chmod -R 755 /var/www/$name"_"gui/"
-    chmod -R 755 /var/www/$name"_"gui/
-    echo -e "cp -fr $path_gui/* /var/www/$name"_"gui/"
-    cp -fr $path_gui/* /var/www/$name"_"gui/
-    #----------Action
-    echo "server {
-    listen $port;
-    server_name _;
-        location /$key {
-            alias /var/www/$name"_"gui;
-            index index.html;
-            try_files \$uri \$uri/ =404;
-        }
-    }" > /etc/nginx/sites-available/$name"_"gui.conf
-    ln -s /etc/nginx/sites-available/$name"_"gui.conf /etc/nginx/sites-enabled/
-    nginx -t
-    systemctl reload nginx
-    systemctl restart nginx
-}
-
-#-----------------------------service_create_interrupt_gpio
-service_create_interrupt_gpio()
-{
-    echo -e "${YELLOW}service_create_interrupt_gpio\n$LINE3${ENDCOLOR}"
-
-    #----------Variable
-    service_name="interrupt_gpio"
-
-    #----------webapi
-    echo -e "${BLUE}$name"_"$service_name${ENDCOLOR}"
-    echo """[Unit]
-    Description=$name"_"$service_name
-
-    [Service]
-    User=root
-    WorkingDirectory=$path/
-    ExecStart=python3 $path/$service_name.py
-    SuccessExitStatus=143
-    TimeoutStopSec=10
-    Restart=on-failure
-    RestartSec=65
-
-    [Install]
-    WantedBy=multi-user.target""" > /etc/systemd/system/$name"_"$service_name.service
-
-    systemctl daemon-reload
-    systemctl enable $name"_"$service_name
-    systemctl restart $name"_"$service_name
-}
-
-#-----------------------------service_create_interrupt_1w
-service_create_interrupt_1w()
-{
-    echo -e "${YELLOW}service_create_interrupt_1w\n$LINE3${ENDCOLOR}"
-
-    #----------Variable
-    service_name="interrupt_1w"
-
-    #----------webapi
-    echo -e "${BLUE}$name"_"$service_name${ENDCOLOR}"
-    echo """[Unit]
-    Description=$name"_"$service_name
-
-    [Service]
-    User=root
-    WorkingDirectory=$path/
-    ExecStart=python3 $path/$service_name.py
-    SuccessExitStatus=143
-    TimeoutStopSec=10
-    Restart=on-failure
-    RestartSec=65
-
-    [Install]
-    WantedBy=multi-user.target""" > /etc/systemd/system/$name"_"$service_name.service
-
-    systemctl daemon-reload
-    systemctl enable $name"_"$service_name
-    systemctl restart $name"_"$service_name
-}
-
-#-----------------------------service_cron_min
-service_cron_min()
-{
-    echo -e "${YELLOW}service_cron_min\n$LINE3${ENDCOLOR}"
-
-    #----------Variable
-    service_name="cron_min"
-
-    #----------Service
-    echo -e "${BLUE}$name"_"$service_name${ENDCOLOR}"
-
-    #----------Service
-    echo """[Unit]
-    Description=$name"_"$service_name"_"service
-
-    [Service]
-    User=root
-    WorkingDirectory=$path/
-    ExecStart=$path/cli.sh webapi_cron_min
-    SuccessExitStatus=143
-    TimeoutStopSec=10
-    Restart=on-failure
-    RestartSec=65
-
-    [Install]
-    WantedBy=multi-user.target""" > /etc/systemd/system/$name"_"$service_name.service
-
-    #----------timer
-    echo "[Unit]
-    Description=$name"_"$service_name"_"timer
-
-    [Timer]
-    OnBootSec=0min
-    OnUnitActiveSec=1min
-    Unit=$name"_"$service_name.service
-
-    [Install]
-    WantedBy=multi-user.target""" > /etc/systemd/system/$name"_"$service_name.timer
-
-    #----------Restart
-    systemctl daemon-reload
-    systemctl enable $name"_"$service_name.timer
-    systemctl restart $name"_"$service_name.timer
-}
-
-#-----------------------------nginx_remove_gui
-nginx_remove_gui()
-{
-    #----------Header
-    echo -e "${YELLOW}nginx_remove_gui\n$LINE3${ENDCOLOR}"
-    #----------Action
-    echo -e "rm -fr /etc/nginx/sites-available/$name"_"gui.conf"
-    rm -fr /etc/nginx/sites-available/$name"_"gui.conf
-    echo -e "rm -fr /etc/nginx/sites-enabled/$name"_"gui.conf"
-    rm -fr /etc/nginx/sites-enabled/$name"_"gui.conf
-    systemctl reload nginx
-    systemctl restart nginx
-}
-
-
-
-#---------------------------------------------------------------------------------Backup
-#-----------------------------backup_sqlite
-backup_sqlite()
-{
-    NOW=$(date +"%Y-%m-%d-%H-%M-%S")
-    echo -e "${YELLOW}${LINE2}Backup database${ENDCOLOR}"
-    echo -e "${BLUE}cp -fr $path/database.db $path/database/sqlite_$NOW${ENDCOLOR}"
-    mkdir -p $path/database
-    cp -fr $path/database.db $path/database/sqlite_$NOW
-}
-
-#-----------------------------github_push
-github_push()
-{
-    NOW=$(date +"%Y-%m-%d %H:%M:%S")
-    echo -e "${YELLOW}${LINE2}github_push${ENDCOLOR}"
-    echo -e "cd $path && git add . && git commit -m "$NOW" && git push"
-    cd $path && git add . && git commit -m "$NOW" && git push
-}
-#-----------------------------github_pull
-github_pull()
-{
-    echo -e "${YELLOW}${LINE2}github_pull${ENDCOLOR}"
-
-    echo -e "${BLUE}cd $path && git pull${ENDCOLOR}"
-    cd $path && git pull
+    systemctl enable $name"_"${service_name}
+    systemctl restart $name"_"${service_name}
 }
--- cli.sh
+++ cli.sh
@@ -32,9 +32,6 @@ menu_main()
         echo -e  ${YELLOW}"1)  ${GREEN}All" ${ENDCOLOR}
         echo -e  ${YELLOW}"2)  ${GREEN}Install" ${ENDCOLOR}
         echo -e  ${YELLOW}"3)  ${GREEN}Service" ${ENDCOLOR}
-        echo -e  ${YELLOW}"4)  ${GREEN}Cron"    ${ENDCOLOR}
-        echo -e  ${YELLOW}"5)  ${GREEN}Backup"  ${ENDCOLOR}
-        echo -e  ${YELLOW}"6)  ${GREEN}Monitor" ${ENDCOLOR}
         echo -e "${YELLOW}${LINE2}${ENDCOLOR}"
         echo -e  ${YELLOW}"q)  ${GREEN}Exit"    ${ENDCOLOR}
         read -p "Enter your choice [1-6]: " choice
@@ -42,9 +39,6 @@ menu_main()
             1)  clear && all;;
             2)  clear && menu_install;;
             3)  clear && menu_service;;
-            4)  clear && menu_cron;;
-            5)  clear && menu_backup;;
-            6)  clear && monitor;;
             q)  clear && exit;;
             *)  exit;;
         esac
@@ -60,23 +54,10 @@ menu_install()
         getHeader
         echo -e  ${YELLOW}${LINE2}Install        ${ENDCOLOR}
         echo -e  ${YELLOW}"1)  ${GREEN}All"      ${ENDCOLOR}
-        echo -e  ${YELLOW}"2)  ${GREEN}Update"   ${ENDCOLOR}
-        echo -e  ${YELLOW}"3)  ${GREEN}Upgrade"  ${ENDCOLOR}
-        echo -e  ${YELLOW}"4)  ${GREEN}Genaral"  ${ENDCOLOR}
-        echo -e  ${YELLOW}"5)  ${GREEN}Nginx"    ${ENDCOLOR}
-        echo -e  ${YELLOW}"6)  ${GREEN}Iptables" ${ENDCOLOR}
-        echo -e  ${YELLOW}"6)  ${GREEN}Postgres" ${ENDCOLOR}
         echo -e  ${YELLOW}${LINE2}               ${ENDCOLOR}
         read -p "Enter your choice [0-7]: " choice
         case $choice in    
             1)  clear && install_all;;
-            2)  clear && install_update;;
-            3)  clear && install_upgrade;;
-            4)  clear && install_general;;
-            5)  clear && install_nginx;;
-            6)  clear && install_iptables;;
-            7)  clear && install_postgres;;
-            q)  clear && menu_main;;
             *)  menu_main;;
         esac
         echo -e "\n"
@@ -95,43 +76,12 @@ menu_service()
         echo -e  ${YELLOW}"2)  ${GREEN}Status"     ${ENDCOLOR}
         echo -e  ${YELLOW}"3)  ${GREEN}Stop"       ${ENDCOLOR}
         echo -e  ${YELLOW}"4)  ${GREEN}Restart"    ${ENDCOLOR}
-        echo -e  ${BLUE}"${LINE1}webapi"           ${ENDCOLOR}
+        echo -e  ${BLUE}"${LINE1}GPIO-subscriber"  ${ENDCOLOR}
         echo -e  ${YELLOW}"5)  ${GREEN}Create"     ${ENDCOLOR}
         echo -e  ${YELLOW}"6)  ${GREEN}Restart"    ${ENDCOLOR}
         echo -e  ${YELLOW}"7)  ${GREEN}Status"     ${ENDCOLOR}
         echo -e  ${YELLOW}"8)  ${GREEN}Monitor"    ${ENDCOLOR}
         echo -e  ${YELLOW}"9)  ${GREEN}Stop"       ${ENDCOLOR}
-        echo -e  ${BLUE}"${LINE1}Nginx"            ${ENDCOLOR}
-        echo -e  ${YELLOW}"10) ${GREEN}Create Api" ${ENDCOLOR}
-        echo -e  ${YELLOW}"11) ${GREEN}Create Gui" ${ENDCOLOR}
-        echo -e  ${YELLOW}"12) ${GREEN}Restart"    ${ENDCOLOR}
-        echo -e  ${YELLOW}"13) ${GREEN}Status"     ${ENDCOLOR}
-        echo -e  ${YELLOW}"14) ${GREEN}Monitor"    ${ENDCOLOR}
-        echo -e  ${YELLOW}"15) ${GREEN}Stop"       ${ENDCOLOR}
-        echo -e  ${BLUE}"${LINE1}Interrupt GPIO"   ${ENDCOLOR}
-        echo -e  ${YELLOW}"16) ${GREEN}Create"     ${ENDCOLOR}
-        echo -e  ${YELLOW}"17) ${GREEN}Restart"    ${ENDCOLOR}
-        echo -e  ${YELLOW}"18) ${GREEN}Status"     ${ENDCOLOR}
-        echo -e  ${YELLOW}"19) ${GREEN}Monitor"    ${ENDCOLOR}
-        echo -e  ${YELLOW}"20) ${GREEN}Stop"       ${ENDCOLOR}
-        echo -e  ${BLUE}"${LINE1}Interrupt 1W"     ${ENDCOLOR}
-        echo -e  ${YELLOW}"21) ${GREEN}Create"     ${ENDCOLOR}
-        echo -e  ${YELLOW}"22) ${GREEN}Restart"    ${ENDCOLOR}
-        echo -e  ${YELLOW}"23) ${GREEN}Status"     ${ENDCOLOR}
-        echo -e  ${YELLOW}"24) ${GREEN}Monitor"    ${ENDCOLOR}
-        echo -e  ${YELLOW}"25) ${GREEN}Stop"       ${ENDCOLOR}
-        echo -e  ${BLUE}"${LINE1}Cron min"         ${ENDCOLOR}
-        echo -e  ${YELLOW}"26) ${GREEN}Create"     ${ENDCOLOR}
-        echo -e  ${YELLOW}"27) ${GREEN}Restart"    ${ENDCOLOR}
-        echo -e  ${YELLOW}"28) ${GREEN}Status"     ${ENDCOLOR}
-        echo -e  ${YELLOW}"29) ${GREEN}Monitor"    ${ENDCOLOR}
-        echo -e  ${YELLOW}"30) ${GREEN}Stop"       ${ENDCOLOR}
-        echo -e  ${BLUE}"${LINE1}Other"            ${ENDCOLOR}
-        echo -e  ${YELLOW}"31) ${GREEN}SApp ON"    ${ENDCOLOR}
-        echo -e  ${YELLOW}"32) ${GREEN}SApp OFF"   ${ENDCOLOR}
-        echo -e  ${YELLOW}"33) ${GREEN}Hotspod ON" ${ENDCOLOR}
-        echo -e  ${YELLOW}"34) ${GREEN}Hotspod OFF"${ENDCOLOR}
-        echo -e  ${YELLOW}"35) ${GREEN}Wifi Connect"${ENDCOLOR}
         echo -e  ${YELLOW}${LINE2}                 ${ENDCOLOR}
         read -p "Enter your choice [1-35]: " choice
         case $choice in
@@ -139,86 +89,13 @@ menu_service()
             2)  clear && service_status_all;;
             3)  clear && service_stop_all;;
             4)  clear && service_restart_all;;
-            5)  clear && service_create_webapi ;;
-            6)  clear && systemctl restart $name"_"webapi.service;;
-            7)  clear && systemctl status $name"_"webapi.service;;
-            8)  clear && journalctl -n 100 -u $name"_"webapi.service -f;;
-            9)  clear && systemctl stop $name"_"webapi.service;;
-            10) clear && service_create_nginx_create_api;;
-            11) clear && service_create_nginx_create_gui;;
-            12) clear && systemctl restart nginx;;
-            13) clear && systemctl status nginx;;
-            14) clear && journalctl -n 100 -u nginx -f;;
-            15) clear && systemctl stop nginx;;
-            16) clear && service_create_interrupt_gpio ;;
-            17) clear && systemctl restart $name"_"interrupt_gpio.service;;
-            18) clear && systemctl status $name"_"interrupt_gpio.service;;
-            19) clear && journalctl -n 100 -u $name"_"interrupt_gpio.service -f;;
-            20) clear && systemctl stop $name"_"interrupt_gpio.service;;
-            21) clear && service_create_interrupt_1w ;;
-            22) clear && systemctl restart $name"_"interrupt_1w.service;;
-            23) clear && systemctl status $name"_"interrupt_1w.service;;
-            24) clear && journalctl -n 100 -u $name"_"interrupt_1w.service -f;;
-            25) clear && systemctl stop $name"_"interrupt_1w.service;;
-            26) clear && service_cron_min ;;
-            27) clear && systemctl restart $name"_"service_cron_min.timer && systemctl restart $name"_"service_cron_min.service;;
-            28) clear && systemctl status $name"_"service_cron_min.service;;
-            29) clear && journalctl -n 100 -u $name"_"service_cron_min.service -f;;
-            30) clear && systemctl stop $name"_"service_cron_min.timer && systemctl stop $name"_"service_cron_min.service;;
-            31) clear && service_start_app_enable ;;
-            32) clear && service_start_app_disable ;;
-            33) clear && service_hotspod_enable ;;
-            34) clear && service_hotspod_disable ;;
-            35) clear && service_wifi_connect ;;
+            5)  clear && service_create_gpio_subscriber ;;
+            6)  clear && systemctl restart $name"_"gpio_subscriber.service;;
+            7)  clear && systemctl status $name"_"gpio_subscriber.service;;
+            8)  clear && journalctl -n 100 -u $name"_"gpio_subscriber.service -f;;
+            9)  clear && systemctl stop $name"_"gpio_subscriber.service;;
             q)  clear && menu_main;;
             *)  menu_main ;;
-        esac
-        echo -e "\n"
-    done
-}
-
-#--------------------menu_cron : 3
-menu_cron()
-{
-    clear
-    while true; do
-        getHeader
-        echo -e  ${YELLOW}${LINE2}Cron        ${ENDCOLOR}
-        echo -e  ${YELLOW}"1) ${GREEN}Daily"  ${ENDCOLOR}
-        echo -e  ${YELLOW}"2) ${GREEN}Hourly" ${ENDCOLOR}
-        echo -e  ${YELLOW}"3) ${GREEN}Minly"  ${ENDCOLOR}
-        echo -e  ${YELLOW}"4) ${GREEN}Backup" ${ENDCOLOR}
-        echo -e  ${YELLOW}${LINE2}            ${ENDCOLOR}
-        read -p "Enter your choice [0-4]: " choice
-        case $choice in            
-            1) clear && cron_daily ;;
-            2) clear && cron_hourly ;;
-            3) clear && cron_minly ;;
-            4) clear && cron_backup ;;
-            q) clear && menu_main ;;
-            *) menu_main ;;
-        esac
-        echo -e "\n"
-    done
-}
-
-#--------------------menu_backup : 4
-menu_backup()
-{
-    clear
-    while true; do
-        getHeader
-        echo -e  ${YELLOW}${LINE2}Bckup${ENDCOLOR}
-        echo -e  ${YELLOW}"1)  ${GREEN}Backup"      ${ENDCOLOR}
-        echo -e  ${YELLOW}"2)  ${GREEN}Push github" ${ENDCOLOR}
-        echo -e  ${YELLOW}"3)  ${GREEN}Restore"     ${ENDCOLOR}
-        echo -e  ${YELLOW}${LINE2}                  ${ENDCOLOR}
-        read -p "Enter your choice [1-3]: " choice
-        case $choice in
-            1) clear && backup_database ;;
-            2) clear && github_push ;;
-            3) clear && restore_database ;;
-            *) menu_main ;;
         esac
         echo -e "\n"
     done
@@ -227,14 +104,7 @@ menu_backup()
 #---------------------------------------------------------------------------------api_interface
 api_interface()
 {
-    if [  "$1" == "webapi_cron_min" ]; then webapi_cron_min; fi
     if [  "$1" == "install" ]; then all; fi
-    if [  "$1" == "install_all" ]; then install_all; fi
-    if [  "$1" == "install_gui" ]; then install_gui; fi
-    if [  "$1" == "service_start_app_enable" ]; then service_start_app_enable; fi
-    if [  "$1" == "service_hotspod_enable" ]; then service_hotspod_enable; fi
-    if [  "$1" == "install_flutter" ]; then install_flutter; fi
-    if [  "$1" == "service_startup" ]; then service_startup; fi
 }
 
 #---------------------------------------------------------------------------------Actions
