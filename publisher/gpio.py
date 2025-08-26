2f9425bcef8b615c9f9ad6969a3f3c3fb9285b30
morteza <kashani.morteza@gmail.com>
2025-08-26 23:47:35 +0330

    ss

--- publisher/gpio.py
+++ publisher/gpio.py
@@ -49,7 +49,7 @@ async def run():
 
     #-------------------------- Listen
     for port in ports : 
-        print(f"{hardware} | Interrupt | {module} | Listen | {port.get('pin')} | {port.get('mode')}")
+        print(f"Interrupt | {module} | Listen | {port.get('pin')} | {port.get('mode')}")
         gpio.setup(port.get("pin"), gpio.IN, pull_up_down=gpio.PUD_DOWN)
         gpio.add_event_detect(port.get("pin"), gpio.BOTH, callback=port_callback, bouncetime=200)
 
